6c1b81182bb677cc2995a45f5e94440900bc9bb3
cuvar <luca-mueller@gmx.net>
2021-11-13 08:46:42 +0100

    fix: remove unnecessary code / comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
 
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className=''>
-      {/* <Navbar /> */}
       <Component {...pageProps} />
-      {/* <Footer /> */}
     </div>
   );
 }
