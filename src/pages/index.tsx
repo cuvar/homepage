9fe173f4279928432b18d4fb09b140be10751796
cuvar <luca-mueller@gmx.net>
2023-09-22 23:19:51 +0200

    fix: center image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <SiteWrapper>
       <div className="flex h-2/3 w-full flex-col items-center justify-center">
         <div className="mx-4 flex w-5/6 flex-col space-y-8 md:w-3/4 lg:w-1/2 xl:w-2/5">
-          <div className="flex flex-col items-center space-x-8 md:flex-row">
+          <div className="flex flex-col items-center md:flex-row md:space-x-8">
             <div className="order-2 flex flex-col space-y-10 md:order-1">
               <h1 className="text-3xl font-bold">Hi, I&apos;m Luca!</h1>
               <p>
