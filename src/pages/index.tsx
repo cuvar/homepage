33df865fe056ec5352166a4b0b8cdb65d96fe43b
cuvar <luca-mueller@gmx.net>
2023-09-22 22:52:13 +0200

    fix: about me

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
           <h1 className="text-3xl font-bold">Hi, I&apos;m Luca!</h1>
           <p>
             I am an aspiring fullstack software engineer with a love for web
-            development. I focus on developing user-facing application, hence,
-            concentrating also on product engineering
+            development. I focus on developing user-facing application for
+            creating helpful products. Currently I work at SAP.
           </p>
           <Links />
         </div>
