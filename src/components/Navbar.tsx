2dad83f2c32d532d5b444fdfe29cb74e1beacf4c
cuvar <luca-mueller@gmx.net>
2023-09-22 18:20:09 +0200

    feat: navbar for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import NavbarLink from "./NavbarLink";
 import type { NavItem } from "~/utils/types";
 import { useRouter } from "next/router";
+import { hamburgerIcon, xIcon } from "~/utils/icons";
+import Link from "next/link";
 
 export default function Navbar() {
   const router = useRouter();
   const { pathname } = router;
   const [activeLink, setActiveLink] = useState(pathname ?? "/");
+  const [showMenu, setShowMenu] = useState(false);
 
   const links: NavItem[] = [
     {
@@ -29,11 +32,12 @@ export default function Navbar() {
 
   function handleClick(item: NavItem) {
     setActiveLink(item.href);
+    if (showMenu) setShowMenu(false);
   }
 
   return (
     <nav className="flex w-full items-center justify-center bg-slate-900 py-6 text-neutral-200">
-      <ul className="flex rounded-full bg-slate-600 px-1 py-2 text-white">
+      <ul className="hidden rounded-full bg-slate-600 px-1 py-2 text-white sm:flex">
         {links.map((link) => (
           <li key={link.text}>
             <NavbarLink
@@ -45,6 +49,40 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
+      <div className="flex w-full flex-col items-end sm:hidden">
+        {!showMenu && (
+          <button onClick={() => setShowMenu(true)} className="mx-4">
+            {hamburgerIcon}
+          </button>
+        )}
+        {showMenu && (
+          <button
+            onClick={() => setShowMenu(false)}
+            className="z-20 mx-4 text-black"
+          >
+            {xIcon}
+          </button>
+        )}
+        {showMenu && (
+          <ul className="absolute top-0 z-10 flex h-screen w-screen flex-col items-center justify-center space-y-8 bg-white text-xl text-black">
+            {links.map((link) => (
+              <li key={link.text}>
+                <Link
+                  href={link.href}
+                  onClick={() =>
+                    handleClick({ text: link.text, href: link.href })
+                  }
+                  className={
+                    activeLink == link.href ? "font-bold text-cgreen-500" : ""
+                  }
+                >
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </nav>
   );
 }
--- src/utils/icons.tsx
+++ src/utils/icons.tsx
@@ -52,3 +52,37 @@ export const mailIcon = (
     />
   </svg>
 );
+
+export const hamburgerIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="h-8 w-8"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+    />
+  </svg>
+);
+
+export const xIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="h-8 w-8"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>
+);
